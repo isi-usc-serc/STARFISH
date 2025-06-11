de233b202ca3ee5acc434300850c03530651095c
griffinmacrae <gmacrae@usc.edu>
2025-06-11 15:26:16 -0700

    thermal data stuff that was never pushed

--- examples/python/mcc134/single_value_read_file_save.py
+++ examples/python/mcc134/single_value_read_file_save.py
@@ -1,27 +1,32 @@
-#!/usr/bin/env python
-#  -*- coding: utf-8 -*-
-"""
-    MCC 134 Functions Demonstrated:
-        mcc134.t_in_read
-
-    Purpose:
-        Read a single data value for each channel in a loop.
-
-    Description:
-        This example demonstrates acquiring data using a software timed loop
-        to read a single value from each selected channel on each iteration
-        of the loop.
-"""
 from __future__ import print_function
 from time import sleep
 from sys import stdout
 from daqhats import mcc134, HatIDs, HatError, TcTypes
 from daqhats_utils import select_hat_device, tc_type_to_string
+import csv
+import datetime
+import os
 
 # Constants
-CURSOR_BACK_2 = '\x1b[2D'
-ERASE_TO_END_OF_LINE = '\x1b[0K'
+EXPORT_FOLDER = "data_exports"
+timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+CSV_FILENAME = os.path.join(EXPORT_FOLDER, f"temperature_data_{timestamp}.csv")
 
+def initialize_csv():
+    """Creates a new CSV file for this run and writes the header."""
+    os.makedirs(EXPORT_FOLDER, exist_ok=True)
+    with open(CSV_FILENAME, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Timestamp", "Trial", "Channel", "Temperature (C)"])
+
+def append_trial_data(trial_number, data):
+    """Appends trial data to the CSV file."""
+    with open(CSV_FILENAME, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        for channel, value in data:
+            writer.writerow([timestamp, trial_number, channel, value])
+    print(f"\nData appended to {CSV_FILENAME}")
 
 def main():
     """
@@ -31,6 +36,8 @@ def main():
     delay_between_reads = 1  # Seconds
     channels = (0, 1, 2, 3)
 
+    initialize_csv()
+    
     try:
         # Get an instance of the selected hat device object.
         address = select_hat_device(HatIDs.MCC_134)
@@ -64,30 +71,33 @@ def main():
                 print('\r{:8d}'.format(samples_per_channel), end='')
 
                 # Read a single value from each selected channel.
+                trial_data = []
                 for channel in channels:
                     value = hat.t_in_read(channel)
                     if value == mcc134.OPEN_TC_VALUE:
                         print('     Open     ', end='')
+                        trial_data.append((channel, "Open"))
                     elif value == mcc134.OVERRANGE_TC_VALUE:
                         print('     OverRange', end='')
+                        trial_data.append((channel, "OverRange"))
                     elif value == mcc134.COMMON_MODE_TC_VALUE:
                         print('   Common Mode', end='')
+                        trial_data.append((channel, "Common Mode"))
                     else:
                         print('{:12.2f} C'.format(value), end='')
+                        trial_data.append((channel, value))
 
                 stdout.flush()
+                append_trial_data(samples_per_channel, trial_data)
 
                 # Wait the specified interval between reads.
                 sleep(delay_between_reads)
 
         except KeyboardInterrupt:
-            # Clear the '^C' from the display.
-            print(CURSOR_BACK_2, ERASE_TO_END_OF_LINE, '\n')
+            print('\nData collection stopped by user.')
 
     except (HatError, ValueError) as error:
         print('\n', error)
 
-
 if __name__ == '__main__':
-    # This will only be run when the module is called directly.
     main()
